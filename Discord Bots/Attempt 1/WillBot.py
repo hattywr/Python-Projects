a19d8444518eafc565bb3d9362bd4eb6b4a5e725
Will <willhatty3@gmail.com>
2023-11-17 14:01:07 -0500

    Adjustments

--- Discord Bots/Attempt 1/WillBot.py
+++ Discord Bots/Attempt 1/WillBot.py
@@ -7,7 +7,7 @@ from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-CHANNEL_ID = os.getenv('CHANNEL_ID')
+#CHANNEL_ID = os.getenv('CHANNEL_ID')
 
 bot  = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
@@ -17,8 +17,8 @@ bot  = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
     print(f'Hello! Wills First Bot is ready for action!')
-    channel = bot.get_channel(int(CHANNEL_ID))
-    await channel.send("Hello! Study Bot is Ready!")
+#   channel = bot.get_channel(int())
+    #await channel.send("Hello! Study Bot is Ready!")
 
 @bot.command()
 async def hello(ctx):
